2f29d3b7b9f190c25532522733cc2e75e0b8bc12
SoanavalonaD <deborah.razafindrabezandrina@esti.mg>
2025-07-23 11:09:41 +0300

    modified data

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import express, { Request, Response } from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 const etudiants = [
-{ id: 1, nom: "Rakoto", prenom: "Jean", filiere: "Informatique" },
-{ id: 2, nom: "Rabe", prenom: "Marie", filiere: "Mathématiques" },
+{ id: 1, nom: "Rakoto", prenom: "Deb", filiere: "Informatique" },
+{ id: 2, nom: "Rabe", prenom: "Rochel", filiere: "Mathématiques" },
 { id: 3, nom: "Ando", prenom: "Lucas", filiere: "Physique" },
 ];
 app.get('/etudiants', (req: Request, res: Response) => {
